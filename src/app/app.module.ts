708ec3edd3c5e5242e70762ad0b87435758ad173
Hamza <hamza.labzioui@gmail.com>
2023-10-05 20:17:06 +0200

    Set up communication between Angular and Electron

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { NgxWigModule } from 'ngx-wig';
 
 import { AppComponent } from './app.component';
 import { EditorComponent } from './editor/editor.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-  EditorComponent
+		EditorComponent
 	],
 	imports: [
 		BrowserModule,
-		NgxWigModule
+		NgxWigModule,
+		FormsModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
